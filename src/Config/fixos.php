59ffbf725de5780945afe4899e22bd53070b486f
CorreiaVitor <vitorcorreia.git@gmail.com>
2023-12-23 01:23:03 -0300

    update

--- src/Config/fixos.php
+++ src/Config/fixos.php
@@ -1,6 +1,6 @@
 <?php
 
 //Variavel que armazena o caminho da pasta do diretório pai do projeto 
-define('PATH', $_SERVER['DOCUMENT_ROOT'] . '/ProjetoOs/controleos_003/src/');
+define('PATH', $_SERVER['DOCUMENT_ROOT'] . '/ProjetoOs/controleos_002/src/');
 
 ?>
